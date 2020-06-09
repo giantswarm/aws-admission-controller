68f4c17ae134ece1077b9922b53d7edd9d9bc576
Pau Rosello <paurosello@gmail.com>
2020-06-09 17:36:01 +0200

    remove test

--- pkg/g8scontrolplane/admit_g8scontrolplane_test.go
+++ pkg/g8scontrolplane/admit_g8scontrolplane_test.go
@@ -1,14 +1,9 @@
 package g8scontrolplane
 
 import (
-	"testing"
-
 	"github.com/ghodss/yaml"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 
 	"github.com/giantswarm/g8s-admission-controller/pkg/admission"
-	"github.com/giantswarm/g8s-admission-controller/pkg/testrunner"
 )
 
 func createG8sControlPlaneAdmitter(config []byte) (admission.Admitter, error) {
@@ -20,13 +15,4 @@ func createG8sControlPlaneAdmitter(config []byte) (admission.Admitter, error) {
 	}
 
 	return NewAdmitter(conf)
-}
-
-func TestG8sControlPlaneAdmission(t *testing.T) {
-	runner := &testrunner.Runner{
-		CreateAdmitter: createG8sControlPlaneAdmitter,
-		Resource:       g8sControlPlaneResource,
-		NewElement:     func() runtime.Object { return &corev1.Node{} },
-	}
-	runner.RunTestcases(t)
 }
