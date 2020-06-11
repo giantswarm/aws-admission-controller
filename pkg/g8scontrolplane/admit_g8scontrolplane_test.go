dcb14370f6618fe5e13040cc34ed24b124ca373e
Nick Jüttner <nick@giantswarm.io>
2020-06-11 18:18:33 +0200

    lint

--- pkg/g8scontrolplane/admit_g8scontrolplane_test.go
+++ pkg/g8scontrolplane/admit_g8scontrolplane_test.go
@@ -8,12 +8,13 @@ import (
 	"testing"
 
 	infrastructurev1alpha2 "github.com/giantswarm/apiextensions/pkg/apis/infrastructure/v1alpha2"
-	"github.com/giantswarm/g8s-admission-controller/pkg/unittest"
 	"k8s.io/api/admission/v1beta1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/giantswarm/g8s-admission-controller/pkg/unittest"
 )
 
 var (
