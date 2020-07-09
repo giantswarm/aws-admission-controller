b8e7cbaad21d3c18dcdc7271a03bfe99124e5c9b
Marian Steinbach <marian@giantswarm.io>
2020-07-09 17:38:01 +0200

    Remove debugging output

--- pkg/awsmachinedeployment/admit_awsmachinedeployment.go
+++ pkg/awsmachinedeployment/admit_awsmachinedeployment.go
@@ -9,13 +9,17 @@ import (
 	"github.com/giantswarm/k8sclient/v3/pkg/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
-	"github.com/google/go-cmp/cmp"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/api/admission/v1beta1"
 	restclient "k8s.io/client-go/rest"
 	apiv1alpha2 "sigs.k8s.io/cluster-api/api/v1alpha2"
 
 	"github.com/giantswarm/admission-controller/pkg/admission"
+)
+
+var (
+	// If not specified otherwise, node pools should have 100% on-demand instances.
+	defaultOnDemandPercentageAboveBaseCapacity int = 100
 )
 
 // Admitter defines our admitter object.
@@ -77,19 +81,14 @@ func (admitter *Admitter) Admit(request *v1beta1.AdmissionRequest) ([]admission.
 		return nil, admission.InternalError
 	}
 
-	log.Infof("Node pool %s - new InstanceDistribution: %#v", awsMachineDeploymentNewCR.ObjectMeta.Name, awsMachineDeploymentNewCR.Spec.Provider.InstanceDistribution)
-
-	if diff := cmp.Diff(awsMachineDeploymentOldCR.Spec.Provider.InstanceDistribution, awsMachineDeploymentNewCR.Spec.Provider.InstanceDistribution); diff != "" {
-		fmt.Printf("AWSMachineDeployment.Spec.Provider.InstanceDistribution changes (-old +new):\n%s", diff)
-	}
-
 	var result []admission.PatchOperation
 
-	// Default the OnDemandPercentageAboveBaseCapacity
+	// Default the OnDemandPercentageAboveBaseCapacity.
+	// Note: This will only work if the incoming CR has the .spec.provider.instanceDistribution
+	// attribute defined. Otherwise the request to create/modify the CR will fail.
 	if awsMachineDeploymentNewCR.Spec.Provider.InstanceDistribution.OnDemandPercentageAboveBaseCapacity == nil {
 		log.Infof("AWSMachineDeployment %s onDemandBaseCapacity is nil and will be set to default 100", awsMachineDeploymentNewCR.ObjectMeta.Name)
-		var defaultVal int = 100
-		patch := admission.PatchReplace("/spec/provider/instanceDistribution/onDemandPercentageAboveBaseCapacity", &defaultVal)
+		patch := admission.PatchReplace("/spec/provider/instanceDistribution/onDemandPercentageAboveBaseCapacity", &defaultOnDemandPercentageAboveBaseCapacity)
 		result = append(result, patch)
 	}
 
