4df7feb60e20d44458baab2b366e6a75c947327b
Christian Bianchi <whites11@gmail.com>
2022-08-04 11:30:58 +0200

    networkpool

--- pkg/aws/v1alpha3/awscluster/validate_awscluster.go
+++ pkg/aws/v1alpha3/awscluster/validate_awscluster.go
@@ -203,7 +203,7 @@ func (v *Validator) Cilium(awsCluster infrastructurev1alpha3.AWSCluster) error {
 
 	if intersect(ciliumIPNet, awsPodIPNet) || intersect(ciliumIPNet, ipamIPNet) {
 		return microerror.Maskf(notAllowedError,
-			fmt.Sprintf("The CIDR from annotation `%s` intersects with the current CIDRs `%s`, `%s`, please specify a different CIDR", annotation.CiliumPodCidr, awsCluster.Spec.Provider.Pods.CIDRBlock, v.ipamCidrBlock),
+			fmt.Sprintf("The CIDR from annotation `%s` intersects with the current CIDRs `%s`, `%s`, please specify a different CIDR", annotation.CiliumPodCidr, awsCluster.Spec.Provider.Pods.CIDRBlock, ipamCidr),
 		)
 
 	}
